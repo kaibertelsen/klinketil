7393d6fab9088935b546c3e9e9f9590913e16e64
Kai Bertelsen <kai@exposoft.no>
2024-11-01 09:09:28 +0100

    ny

--- admin-loader.js
+++ admin-loader.js
@@ -10,7 +10,7 @@ function loadScript(url) {
 
 // Liste over CDN-URL-er som skal lastes inn
 const cdnScripts = [
-    "https://kaibertelsen.github.io/klinketil/admin-afterclick-functions.js"
+    "https://kaibertelsen.github.io/klinketil/adminpages/admin-afterclick-functions.js"
 ];
 
 // Laste inn alle skriptene sekvensielt
