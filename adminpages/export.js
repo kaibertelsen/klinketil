2102924d0db1914167fd652f02937f778de12568
Kai Bertelsen <kai@exposoft.no>
2025-09-04 10:39:08 +0200

    ny

--- adminpages/export.js
+++ adminpages/export.js
@@ -178,9 +178,7 @@ function findKeysforExportonKlient(data){
     return ["Nr","Dato","Navn","Kommentar", "Gruppe", "Prosjekt", "Timer","Antall enheter","Alder","F.dato","Kontonr","E-post", "Godkjent", "Fakturert", "Person ID"];
     }else if(kjontrollnr == "b7fc"){
     //BPS
-    //dob skal inn i eksport
-    mergeDobInTimeList(data,totalusers);
-
+  
     return ["Nr","Dato","Navn","Kommentar","Prosjekt", "Timer","Antall enheter","Alder","F.dato", "Godkjent", "Fakturert", "E-post", "Person ID"];
     }else if(kjontrollnr == "cfbb"){
     //Måløy
